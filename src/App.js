3b61159329d7bb9bc1f905e2d5999a5d1f25b9bc
mdaniyalaslam <mdaniyalaslam@gmail.com>
2017-12-19 01:14:35 +0500

    todo2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      todo : ''
+
+    }
+    this.todoHandler = this.todoHandler.bind(this)
+  }
+
+  todoHandler(ev){
+    this.setState({
+      todo : ev.target.value
+    })
+  }
+
   render() {
     return (
+        
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Todo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+        <input type="text" className="form-control field" placeholder="Write Todo" name="todo" value={this.state.todo} onChange = {this.todoHandler}/>
+        <button type="button" className="btn btn-primary">Add</button>
+        
       </div>
     );
   }
